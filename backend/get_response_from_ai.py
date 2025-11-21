67ac99291a48f4461158ed73f52acafded06022f
pmpradee <maram.pradeepsaireddy@amadeus.com>
2025-11-21 15:07:24 +0530

    Genai generate content error fix

--- backend/get_response_from_ai.py
+++ backend/get_response_from_ai.py
@@ -112,7 +112,7 @@ def get_ai_response_func(prompt:str, image=None,gender:str="men"):
                 Be friendly, brief, and encouraging."""
             full_prompt = f"{system_instruction}\nUser: {prompt}\nAssistant:"
             model = genai.GenerativeModel(
-                model_name='gemini-2.0-flash-exp')
+                model_name='gemini-2.5-flash')
             response = model.generate_content(full_prompt)
             print(f"AI Response: {response.text}")
             return response.text 
